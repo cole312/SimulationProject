b72deba6e053e260771d16417ab16d894ed58b15
cole312 <choover6@uwo.ca>
2026-06-24 12:12:53 -0400

    added position to config file2

--- src/substrate.py
+++ src/substrate.py
@@ -2,11 +2,16 @@ import trimesh
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import tomli
 
 #Name mesh here
-name = 'test'
 
-os.mkdir(f'substrate/{name}')
+with open("sim_configs/test_config.toml", "rb") as f:
+    config = tomli.load(f)
+
+name = config["substrate"]["name"]
+
+os.mkdir(f'substrate/{name}',exist_ok=True)
 
 #Create mesh here
 mesh = trimesh.creation.cylinder(radius=1, height=10)
@@ -26,14 +31,15 @@ ax.plot_trisurf(
 )
 
 ax.set_box_aspect([1, 1, 1])
-plt.savefig(f'substrate/{name}/{name}_mesh', dpi= 300, bbox_inches='tight')
+plt.savefig(f'substrate/{name}/{name}_mesh.png', dpi= 300, bbox_inches='tight')
+plt.close(fig)
 
 #csv export
 data_verticies = pd.DataFrame(mesh.vertices, columns=['x','y','z'])
-data_verticies.to_csv(f'substrate/{name}/{name}_vertices', index=False)
+data_verticies.to_csv(f'substrate/{name}/{name}_vertices.csv', index=False)
 
 data_faces = pd.DataFrame(mesh.faces, columns=['v1','v2','v3'])
-data_faces.to_csv(f'substrate/{name}/{name}_faces', index=False)
+data_faces.to_csv(f'substrate/{name}/{name}_faces.csv', index=False)
 
 
 
