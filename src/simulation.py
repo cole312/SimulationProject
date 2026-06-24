31ffd93f887afa1db09ee47d6bf94458e1d8e9ea
cole312 <choover6@uwo.ca>
2026-06-24 11:53:34 -0400

    changed signal output to csv file

--- src/simulation.py
+++ src/simulation.py
@@ -1,11 +1,11 @@
 import numpy as np
 from scipy.spatial.transform import Rotation as R
-import trimesh
 import matplotlib.pyplot as plt
 from disimpy import gradients, simulations, substrates
 from dipy.core.sphere import fibonacci_sphere
 import pandas as pd
 import tomli
+import csv
 
 with open("sim_configs/test_config.toml", "rb") as f:
     config = tomli.load(f)
@@ -21,11 +21,11 @@ directions = config["waveform"]["num_directions"]
 b_num = config["waveform"]["num_b"]
 
 
-def get_substrate(meshName, folder):
+def get_substrate(meshName):
 
     print(meshName)
-    data_verts = pd.read_csv(f'outputs/{folder}/{meshName}_vertices')
-    data_faces = pd.read_csv(f'outputs/{folder}/{meshName}_faces')
+    data_verts = pd.read_csv(f'substrate/{meshName}/{meshName}_vertices')
+    data_faces = pd.read_csv(f'substrate/{meshName}/{meshName}_faces')
 
     vertices = data_verts.to_numpy()
     faces = data_faces.to_numpy()
@@ -65,171 +65,90 @@ def read_shape(filename):
     return x_grad,y_grad,z_grad
 
 
-substrate = get_substrate(meshName, folder)
+substrate = get_substrate(meshName)
 
-shape_signals = []
+#waveform, x, y, z, b value, 
 
-for filecount, file in enumerate(waveforms):
+csv_filename=f"outputs/{meshName}_signals"
+with open(csv_filename, mode="w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["waveform","x","y","z","bval","signal"])
 
-    x_grad, y_grad, z_grad = read_shape(file)
-    
-    time = len(x_grad)*0.02
-    time_points = np.arange(0,time,0.02)
+    shape_signals = []
+    for filecount, file in enumerate(waveforms):
 
-    gradient = np.zeros([1,len(time_points),3])
-
-    #Store the x,y,z data into a single gradient file. 
-    gradient[0,:,0] = x_grad
-    gradient[0,:,1] = y_grad
-    gradient[0,:,2] = z_grad
-
-    gradient *= 1e-3
-
-    print(f"Bval: {(gradients.calc_b(gradient,0.02e-3)*1e-6)[0]:.0f}")
-
-    #30 equally spaced points around a sphere. These are target vectors for the rotation
-    bvecs = fibonacci_sphere(n_points=directions)
-
-
-    gradient_final = np.zeros([len(bvecs), len(time_points), 3])
-
-    for i in range(0, len(bvecs)):
-
-        target = bvecs[i]
-
-        rotation, _ = R.align_vectors([target], [1,0,0])
-        rot_matrix = rotation.as_matrix()
-
-        rot_waveform = gradient @ rot_matrix.T
-
-        gradient_final[i, : , : ] = rot_waveform
-
-    #Interpolate gradient to variable step size (n_t), dt is the new time step
-    gradient_final, dt = gradients.interpolate_gradient(gradient_final, 0.02e-3, int(n_t))
-    
-    b_base = (gradients.calc_b(gradient_final, dt) * 1e-6)
-    print(f"B-base: {b_base[0]}")
-
-    signals = []
-    b_targets = np.linspace(0, 4500, b_num) 
-
-    #Scale gradient values to achieve different b's. Scale is calcuated via b_base
-    for j, b in enumerate(b_targets):
-        print(f"\n\nGradient: {filecount + 1} / {len(waveforms)}. B value: {j} / {len(b_targets)}\n\n")
-        if b == 0:
-            signals.append(n_walkers) 
-            continue
-
-        scale = np.sqrt(b / b_base[0])
-        scaled_gradient = gradient_final * scale
-
-        #Simulate with the new scaled gradient. All signals are stored in shape_signals
-
-        #traj_file = "example_traj.txt"
-        signal = simulations.simulation(
-        n_walkers=int(n_walkers),
-        diffusivity=diffusivity,
-        gradient=scaled_gradient,
-        dt=dt,
-        substrate=substrate,
-        #traj=traj_file,
-        )
+        x_grad, y_grad, z_grad = read_shape(file)
         
-        signals.append(np.mean(signal))
+        time = len(x_grad)*0.02
+        time_points = np.arange(0,time,0.02)
 
-    signals = [abs(signal / n_walkers) for signal in signals]
+        gradient = np.zeros([1,len(time_points),3])
 
-    shape_signals.append(signals)
+        #Store the x,y,z data into a single gradient file. 
+        gradient[0,:,0] = x_grad
+        gradient[0,:,1] = y_grad
+        gradient[0,:,2] = z_grad
 
-log_signals = np.log(shape_signals)
+        gradient *= 1e-3
 
-md = []
-kurt = []
-curvesA = []
-curvesB = []
-curvesC = []
+        print(f"Bval: {(gradients.calc_b(gradient,0.02e-3)*1e-6)[0]:.0f}")
 
-for i in range(len(waveforms)):
-    A, B, C = np.polyfit(b_targets, log_signals[i], 2)
-    D = -B
-    md.append(D)
-    kurt.append((6 * A) / (D**2))
-    curvesA.append(A)
-    curvesB.append(B)
-    curvesC.append(C)
-
-waveforms = [
-    "LTE 0HZ",
-    "LTE 50HZ",
-    "LTE 100Hz",
-    "STE iso",
-    "STE Aniso"
-]
-
-fig, ax = plt.subplots(figsize=(8, 5))
-x = np.linspace(0,4500,5)
-
-for i, signal_list in enumerate(shape_signals):
-    y = curvesA[i]*x**2 + curvesB[i]*x +curvesC[i]   
-    ax.plot(x,np.exp(y))
-    ax.scatter(b_targets, signal_list, marker='o',linestyle='--', label=f"{waveforms[i]}, MD: {md[i]:.4e}, K: {kurt[i]:.4f}")
-
-ax.set_yscale('log')
-ax.set_xlabel("b values")
-ax.set_ylabel("Normalized Signal")
-ax.grid(True, which="both", linestyle='--', alpha=0.5)
-ax.legend()
-plt.savefig(f"outputs/{folder}/{meshName}_signals_{n_walkers}walkers_{n_t}steps.png")
-plt.show()
-
-fig, ax = plt.subplots(figsize=(8, 5))
-
-freq = np.array([0,50,100])
-diff = np.array([md[0]*1e3,md[1]*1e3,md[2]*1e3])
-
-ax.scatter(freq, diff, color='red',label='MD Data Points')
-
-x1 = np.linspace(0,200,99)
-
-lin_params = np.polyfit(freq, diff, 1)
-lin_fit = lin_params[0] * freq + lin_params[1]
-lin_error = np.sum((diff - lin_fit) ** 2)
-
-sqrt_params = np.polyfit(np.sqrt(freq), diff, 1)
-sqrt_fit= sqrt_params[0] * np.sqrt(freq) + sqrt_params[1]
-sqrt_error = np.sum((diff - sqrt_fit) ** 2)
+        #30 equally spaced points around a sphere. These are target vectors for the rotation
+        bvecs = fibonacci_sphere(n_points=directions)
 
 
-sq_params = np.polyfit(freq**2, diff, 1)
-sq_fit= sq_params[0] * freq**2 + sq_params[1]
-sq_error = np.sum((diff - sq_fit) ** 2)
+        gradient_final = np.zeros([len(bvecs), len(time_points), 3])
 
-if sqrt_error < lin_error and sqrt_error < sq_error:
-    y1 = sqrt_params[0] * np.sqrt(x1) + sqrt_params[1]
-    y2 = lin_params[0] * x1 + lin_params[1]
-    y3 = sq_params[0] * (x1**2) + sq_params[1]
-    print("Best Fit: Square Root")
-    
-elif lin_error < sqrt_error and lin_error < sq_error:
+        for i in range(0, len(bvecs)):
 
-    y1 = lin_params[0] * x1 + lin_params[1]
-    y2 = sqrt_params[0] * np.sqrt(x1) + sqrt_params[1]
-    y3 = sq_params[0] * (x1**2) + sq_params[1]
-    print("Best Fit: Linear")
-    
-else:
-    y1 = sq_params[0] * (x1**2) + sq_params[1]
-    y2 = sqrt_params[0] * np.sqrt(x1) + sqrt_params[1]
-    y3 = lin_params[0] * x1 + lin_params[1]
-    print("Best Fit: Squared")
+            target = bvecs[i]
 
-ax.set_ylabel("MD")
-ax.set_xlabel("Hz")
-plt.plot(x1,y1)
-plt.plot(x1,y2, ls= ":", alpha = 0.7)
-plt.plot(x1,y3, ls = ':', alpha= 0.7)
+            rotation, _ = R.align_vectors([target], [1,0,0])
+            rot_matrix = rotation.as_matrix()
 
-plt.savefig(f"outputs/{folder}/{meshName}_Md_F_graph.png")
+            rot_waveform = gradient @ rot_matrix.T
+
+            gradient_final[i, : , : ] = rot_waveform
+
+        #Interpolate gradient to variable step size (n_t), dt is the new time step
+        gradient_final, dt = gradients.interpolate_gradient(gradient_final, 0.02e-3, int(n_t))
+        
+        b_base = (gradients.calc_b(gradient_final, dt) * 1e-6)
+        print(f"B-base: {b_base[0]}")
+
+        signals = []
+        b_targets = np.linspace(0, 4500, b_num) 
+
+        #Scale gradient values to achieve different b's. Scale is calcuated via b_base
+        for j, b in enumerate(b_targets):
+            print(f"\n\nGradient: {filecount + 1} / {len(waveforms)}. B value: {j} / {len(b_targets)}\n\n")
+            if b == 0:
+                signals.append(n_walkers) 
+                continue
+
+            scale = np.sqrt(b / b_base[0])
+            scaled_gradient = gradient_final * scale
+
+            #Simulate with the new scaled gradient. All signals are stored in shape_signals
+
+            #traj_file = "example_traj.txt"
+            signal = simulations.simulation(
+            n_walkers=int(n_walkers),
+            diffusivity=diffusivity,
+            gradient=scaled_gradient,
+            dt=dt,
+            substrate=substrate,
+            #traj=traj_file,
+            )
+            
+            # Loop through each direction to log its specific x, y, z component and signal.
+            for i, bvec in enumerate(bvecs):
+
+                norm_signal = abs(signal / n_walkers)
+                
+                # Write a row to the CSV file
+                writer.writerow([filecount+1, bvec[0], bvec[1], bvec[2], b, norm_signal[i]])
+
 
 
 
--- src/substrate.py
+++ src/substrate.py
@@ -6,13 +6,10 @@ import os
 #Name mesh here
 name = 'test'
 
-
-folder = (f'{name}_outputs')
-os.mkdir(f'outputs/{folder}')
+os.mkdir(f'substrate/{name}')
 
 #Create mesh here
-mesh = []
-mesh.apply_scale(1e-6)
+mesh = trimesh.creation.cylinder(radius=1, height=10)
 
 #Plotting mesh.
 fig = plt.figure()
@@ -29,14 +26,14 @@ ax.plot_trisurf(
 )
 
 ax.set_box_aspect([1, 1, 1])
-plt.savefig(f'outputs/{folder}/{name}_mesh', dpi= 300, bbox_inches='tight')
+plt.savefig(f'substrate/{name}/{name}_mesh', dpi= 300, bbox_inches='tight')
 
 #csv export
 data_verticies = pd.DataFrame(mesh.vertices, columns=['x','y','z'])
-data_verticies.to_csv(f'outputs/{folder}/{name}_vertices', index=False)
+data_verticies.to_csv(f'substrate/{name}/{name}_vertices', index=False)
 
 data_faces = pd.DataFrame(mesh.faces, columns=['v1','v2','v3'])
-data_faces.to_csv(f'outputs/{folder}/{name}_faces', index=False)
+data_faces.to_csv(f'substrate/{name}/{name}_faces', index=False)
 
 
 
