a3a9d88ebd594eab15e3af80efb55a6017f5f728
cole312 <choover6@uwo.ca>
2026-06-25 09:35:01 -0400

    add a few basic substrates

--- src/substrate.py
+++ src/substrate.py
@@ -3,18 +3,30 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import tomli
+import numpy as np
 
-#Name mesh here
 
-with open("sim_configs/test_config.toml", "rb") as f:
-    config = tomli.load(f)
 
-name = config["substrate"]["name"]
+#with open("sim_configs/test_config.toml", "rb") as f:
+    #config = tomli.load(f)
 
-os.mkdir(f'substrate/{name}',exist_ok=True)
+#name = config["substrate"]["name"]
+name = "multiCylinder_radius10_height_500"
+os.makedirs(f'substrate/{name}',exist_ok=True)
 
-#Create mesh here
-mesh = trimesh.creation.cylinder(radius=1, height=10)
+mesh8 = trimesh.creation.cylinder(1,50)
+mesh2 = (trimesh.creation.cylinder(1,50)).apply_translation([3,0,0])
+mesh2.apply_transform(trimesh.transformations.rotation_matrix(np.radians(45), [1,0,0]))
+mesh3 = (trimesh.creation.cylinder(1,50)).apply_translation([0,3,0])
+mesh3.apply_transform(trimesh.transformations.rotation_matrix(np.radians(60), [1,1,0]))
+mesh4 = (trimesh.creation.cylinder(1,50)).apply_translation([0,-3,0])
+mesh4.apply_transform(trimesh.transformations.rotation_matrix(np.radians(30), [1,-1,0]))
+mesh5 = (trimesh.creation.cylinder(1,50)).apply_translation([-3,0,2])
+mesh5.apply_transform(trimesh.transformations.rotation_matrix(np.radians(90), [1,0,0]))
+
+mesh = trimesh.util.concatenate([mesh8,mesh2,mesh3,mesh4,mesh5])
+
+#end mesh
 
 #Plotting mesh.
 fig = plt.figure()
@@ -30,7 +42,7 @@ ax.plot_trisurf(
     linewidth=0.5
 )
 
-ax.set_box_aspect([1, 1, 1])
+ax.axis('equal')
 plt.savefig(f'substrate/{name}/{name}_mesh.png', dpi= 300, bbox_inches='tight')
 plt.close(fig)
 
