f7520e306035d2b8ab900f830d7fa69def7b2434
cole312 <choover6@uwo.ca>
2026-06-29 12:12:21 -0400

    Create test_job.sh for SLURM job submission
    Add a SLURM job script for running simulations

---
+++ test_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=testslurm 
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-task=1 
+#SBATCH --mem=16G
+#SBATCH --output=slurm-%j.out
+
+#SBATCH --partition=hx
+
+# Run computation
+cd /nfs/scratch/choover/SimulationProject/
+pixi run python src/simulation.py sim_configs/test_config.toml
+
+
