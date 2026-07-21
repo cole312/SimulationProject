6c20c35be22bd19de03ba6ce2db6b2bfbaaded91
cole312 <choover6@uwo.ca>
2026-07-21 11:23:57 -0400

    add batch files for new rots

---
+++ batch/euler_intra_GFO.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=testslurm 
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --gpus-per-node=1 
+#SBATCH --mem=16G
+#SBATCH --output=/nfs/scratch/choover/SimulationProject2/slurm_outputs/slurm-%j.out
+#SBATCH --partition=hx
+
+# Run computation
+cd /nfs/scratch/choover/SimulationProject2/
+
+pixi run python src/simulation_euler.py sim_configs/config_euler_intra_GFO.toml
+
+
