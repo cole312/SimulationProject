c4764519c7fc0d9c11014dc0694fa0ae9f644765
cole312 <choover6@uwo.ca>
2026-07-29 11:33:09 -0400

    imporved batch files

--- batch/extra_euler.sh
+++ batch/extra_euler.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=noneuler 
+#SBATCH --job-name=ext_eul
 #SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=10
 #SBATCH --gpus-per-node=1 
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --output=/nfs/scratch/choover/SimulationProject2/slurm_outputs/slurm-%j.out
 #SBATCH --partition=hx
 
 # Run computation
 cd /nfs/scratch/choover/SimulationProject2/
 
-pixi run python src/simulation.py sim_configs/noneuler_config.toml
+pixi run python src/Simulation.py extra_euler_config.toml
 
 
--- batch/intra_euler.sh
+++ batch/intra_euler.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=int_GFO_t 
+#SBATCH --job-name=int_eul
 #SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=10
 #SBATCH --gpus-per-node=1 
-#SBATCH --mem=128G
+#SBATCH --mem=64G
 #SBATCH --output=/nfs/scratch/choover/SimulationProject2/slurm_outputs/slurm-%j.out
 #SBATCH --partition=hx
 
 # Run computation
 cd /nfs/scratch/choover/SimulationProject2/
 
-pixi run python src/Simulation.py euler_config.toml
+pixi run python src/Simulation.py intra_euler_config.toml
 
 
