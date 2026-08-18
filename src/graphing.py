5f1dd4119e5aa7d0e24f8ba9bb03d7b17d11e2d2
cole312 <choover6@uwo.ca>
2026-08-18 13:11:20 -0400

    add variance term and graph kurt md and V wrt HZ

--- src/graphing.py
+++ src/graphing.py
@@ -48,14 +48,18 @@ for wf in unique_waveforms:
     b_mask = b_arr[mask]
     signals_mask = signals_arr[mask]
 
-    A, B, C = np.polyfit(b_mask, np.log(signals_mask), 2)
+    A, B, C = np.polyfit(b_mask/1000, np.log(signals_mask), 2)
     if abs(A) < 1e-9:
         A = 0
-
-
     D = -B
+
+    V = []
+    V.append(A*2)
+
+    # v is a*2
+    #plot v and k with f
     kurt.append((6 * A) / (D**2))
-    md.append(D*1000)
+    md.append(D)
 
     curvesA.append(A)
     curvesB.append(B)
@@ -68,7 +72,7 @@ for wf in unique_waveforms:
     y_fit = np.exp(A * (x_fit**2) + B * x_fit + C)
 
     ax.scatter(b_values, signals, marker='o', label=f"{label_name} Data")
-    ax.plot(x_fit, y_fit, linestyle='--', label=f"Fit (MD: {md[-1]:.4e}, K: {kurt[-1]:.4f})")
+    ax.plot(x_fit, y_fit, linestyle='--', label=f"Fit (MD: {md[-1]:.4e}, K: {kurt[-1]:.4f}, V: {V[-1]})")
 
 ax.set_yscale('log')
 ax.set_xlabel("b-value ($s/mm^2$)")
@@ -86,52 +90,57 @@ fig, ax = plt.subplots(figsize=(8, 5))
 
 freq = np.array([0, 50, 100])
 diff = np.array([md[0], md[1], md[2]])
+kurt = np.array([kurt[0], kurt[1], kurt[2]])
+variance = np.array([V[0],V[1],V[2]])
 print([md[0], md[1], md[2]])
 
-ax.scatter(freq, diff, color='red', marker='o', s=10, zorder=5, label='MD Data Points')
+paramList = [("Diffusivity",diff), ("Kurtosis", kurt), ("Variance",variance)]
 
-x1 = np.linspace(0, 200, 100)
+for name, param in paramList:
+    ax.scatter(param, diff, color='red', marker='o', s=10, zorder=5, label='MD Data Points')
 
-lin_params = np.polyfit(freq, diff, 1)
-lin_fit_vals = lin_params[0] * freq + lin_params[1]
-lin_error = np.sum((diff - lin_fit_vals) ** 2)
-y_linear = lin_params[0] * x1 + lin_params[1]
+    x1 = np.linspace(0, 200, 100)
 
-sqrt_params = np.polyfit(np.sqrt(freq), diff, 1)
-sqrt_fit_vals = sqrt_params[0] * np.sqrt(freq) + sqrt_params[1]
-sqrt_error = np.sum((diff - sqrt_fit_vals) ** 2)
-y_sqrt = sqrt_params[0] * np.sqrt(x1) + sqrt_params[1]
+    lin_params = np.polyfit(param, diff, 1)
+    lin_fit_vals = lin_params[0] * param + lin_params[1]
+    lin_error = np.sum((diff - lin_fit_vals) ** 2)
+    y_linear = lin_params[0] * x1 + lin_params[1]
 
-sq_params = np.polyfit(freq**2, diff, 1)
-sq_fit_vals = sq_params[0] * freq**2 + sq_params[1]
-sq_error = np.sum((diff - sq_fit_vals) ** 2)
-y_squared = sq_params[0] * (x1**2) + sq_params[1]
+    sqrt_params = np.polyfit(np.sqrt(param), diff, 1)
+    sqrt_fit_vals = sqrt_params[0] * np.sqrt(param) + sqrt_params[1]
+    sqrt_error = np.sum((diff - sqrt_fit_vals) ** 2)
+    y_sqrt = sqrt_params[0] * np.sqrt(x1) + sqrt_params[1]
 
-errors = {"Square Root": sqrt_error, "Linear": lin_error, "Squared": sq_error}
-best_fit_name = min(errors, key=errors.get)
-print(f"Best Fit: {best_fit_name}")
+    sq_params = np.polyfit(param**2, diff, 1)
+    sq_fit_vals = sq_params[0] * param**2 + sq_params[1]
+    sq_error = np.sum((diff - sq_fit_vals) ** 2)
+    y_squared = sq_params[0] * (x1**2) + sq_params[1]
 
-ls_squared = '-' if best_fit_name == "Squared" else ':'
-ls_linear  = '-' if best_fit_name == "Linear" else ':'
-ls_sqrt    = '-' if best_fit_name == "Square Root" else ':'
+    errors = {"Square Root": sqrt_error, "Linear": lin_error, "Squared": sq_error}
+    best_fit_name = min(errors, key=errors.get)
+    print(f"Best Fit: {best_fit_name}")
 
-ax.plot(x1, y_squared, color='#004949', linestyle=ls_squared, linewidth=2, 
+    ls_squared = '-' if best_fit_name == "Squared" else ':'
+    ls_linear  = '-' if best_fit_name == "Linear" else ':'
+    ls_sqrt    = '-' if best_fit_name == "Square Root" else ':'
+
+    ax.plot(x1, y_squared, color='#004949', linestyle=ls_squared, linewidth=2, 
         label=f'Squared Fit {"(Best)" if best_fit_name == "Squared" else ""}')
 
-ax.plot(x1, y_linear, color='#FF6B6B', linestyle=ls_linear, linewidth=2, 
+    ax.plot(x1, y_linear, color='#FF6B6B', linestyle=ls_linear, linewidth=2, 
         label=f'Linear Fit  {"(Best)" if best_fit_name == "Linear" else ""}')
 
-ax.plot(x1, y_sqrt, color='#009999', linestyle=ls_sqrt, linewidth=2, 
+    ax.plot(x1, y_sqrt, color='#009999', linestyle=ls_sqrt, linewidth=2, 
         label=f'Square Root Fit {"(Best)" if best_fit_name == "Square Root" else ""}')
 
-ax.set_ylabel("Mean Diffusivity (MD)")
-ax.set_xlabel("Frequency (Hz)")
-ax.set_title(f"Frequency Dependence")
-ax.grid(True, which="both", linestyle='--', alpha=0.5)
-ax.legend(loc='best')
+    ax.set_ylabel("Mean Diffusivity (MD)")
+    ax.set_xlabel("Frequency (Hz)")
+    ax.set_title(f"Frequency Dependence")
+    ax.grid(True, which="both", linestyle='--', alpha=0.5)
+    ax.legend(loc='best')
 
-plt.tight_layout()
-plt.savefig(f"{output}/{graph_title}_fmd.png", dpi=300)
+    plt.tight_layout()
+    plt.savefig(f"{output}/{graph_title}_{name}.png", dpi=300)
 
 try:
     traj_file = f"outputs/traj_{graph_title}"
@@ -157,8 +166,7 @@ try:
 
         # ax.scatter(traj[i,:,0],traj[i,:,1],traj[i,:,2],s=1)
 
-    ax.view_init(elev=90, azim=70)
-
+    ax.view_init(elev=0, azim=0)
     ax.set_title("Disimpy Trajectories")
     ax.set_ylabel("Y")
     ax.set_xlabel("X")
