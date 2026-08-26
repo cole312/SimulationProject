a046a3094240faa98424a623c7fa4df827bac44a
cole312 <choover6@uwo.ca>
2026-08-26 14:45:38 -0400

    add sub

--- substrate/!_Gen/substrateCylinder.py
+++ substrate/!_Gen/substrateCylinder.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-spread = 0
-n_cylinders =8000
+spread = 0.75
+n_cylinders =6000
 cyl_height = 1000
 domain_size = 120
 gamma = False
