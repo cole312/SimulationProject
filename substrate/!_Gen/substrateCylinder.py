9540a9ff217cf772b37689b2852e497f7b983b2d
cole312 <choover6@uwo.ca>
2026-08-27 10:56:08 -0400

    add 2 subs

--- substrate/!_Gen/substrateCylinder.py
+++ substrate/!_Gen/substrateCylinder.py
@@ -4,15 +4,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-spread = 0.75
+spread = 0.4
 n_cylinders =3000
 cyl_height = 1000
 domain_size = 120
 gamma = True
 gamma_shape = 0.75
 gamma_scale = 0.55
-max_radius = 2.5
-min_radius = 0.7
+max_radius = 3
+min_radius = 0.2
 cyl_radius = 0.5
 
 if gamma:
